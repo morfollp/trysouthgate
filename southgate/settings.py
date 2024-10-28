89be3afb6b3aebd32f0062534f4cfedde2c95f5b
morfollp <morfofintechllp@gmail.com>
2024-10-29 02:55:23 +0530

    Update settings.py

--- southgate/settings.py
+++ southgate/settings.py
@@ -128,9 +128,9 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 STATICFILES_DIRS = (
-    BASE_DIR / "staticfiles",
+    BASE_DIR / "static",
 )
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
