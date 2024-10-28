7876b8115778924f0f9cc89e59786051da82752a
morfollp <morfofintechllp@gmail.com>
2024-10-29 03:47:15 +0530

    Update settings.py

--- southgate/settings.py
+++ southgate/settings.py
@@ -125,12 +125,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'southgate/static',
+]
+
+# media files configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
-STATICFILES_DIRS = (
-    BASE_DIR / "southgate/static",
-)
-STATIC_ROOT = '/opt/render/project/src/staticfiles'
+MEDIA_ROOT = BASE_DIR /'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
