a622301c677a470d04574de80e5621b546ec09f6
morfollp <morfofintechllp@gmail.com>
2024-10-29 02:25:03 +0530

    Update settings.py

--- southgate/settings.py
+++ southgate/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    '/opt/render/project/src/static',
+os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
