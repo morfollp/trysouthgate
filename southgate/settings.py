20f28b32bef4d0e5e120dbfdd6c4d068c5af9e2d
morfollp <morfofintechllp@gmail.com>
2024-10-29 00:46:22 +0530

    Update settings.py

--- southgate/settings.py
+++ southgate/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     '/opt/render/project/src/static',
 ]
