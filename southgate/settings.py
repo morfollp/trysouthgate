26f410bbc28da132ca1f4c52622d5cdcdc3a22d0
morfollp <morfofintechllp@gmail.com>
2024-10-28 23:33:41 +0530

    Update settings.py

--- southgate/settings.py
+++ southgate/settings.py
@@ -66,7 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-'django.core.context_processors.static'
+'django.core.context_processors.static',
  'django.contrib.messages.context_processors.messages',
             ],
         },
