e749ad8540796a55046e97a1a8993117b09fcc58
morfollp <morfofintechllp@gmail.com>
2024-10-29 13:36:32 +0530

    Update urls.py

--- southgate/urls.py
+++ southgate/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('securelogin/', admin.site.urls),
     
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
