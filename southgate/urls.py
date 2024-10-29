b37f1b4bf262ad3422742f82b191aade8139f4ad
morfollp <morfofintechllp@gmail.com>
2024-10-29 13:50:57 +0530

    bug fix

--- southgate/urls.py
+++ southgate/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('securelogin/', admin.site.urls),
     
     
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
