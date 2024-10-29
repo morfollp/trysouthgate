7215e02e8f1c6d904e38d5de354c3d3ce331dbcd
morfollp <morfofintechllp@gmail.com>
2024-10-29 13:42:05 +0530

    Update settings.py

--- southgate/settings.py
+++ southgate/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'southgate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR /'templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
